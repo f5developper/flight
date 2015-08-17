55901cb04ff323015785cadbc78ed82f9df1d1a0
f5 <f5developper@gmail.com>
2015-08-17 23:56:16 +0900

    検索処理実装

--- flight_query.js
+++ flight_query.js
@@ -1,23 +1,47 @@
+Air_lines = new Mongo.Collection("air_lines");
+Flight_info = new Mongo.Collection("flight_info");
+
+if (Meteor.isServer) {
+    Meteor.publish('air_lines', function () {
+        return Air_lines.find({}, {sort: {priority: -1}});
+    });
+
+    Meteor.publish('flight_info', function () {
+        return Flight_info.find();
+    });
+}
+
 
 if (Meteor.isClient) {
+
+    $(function () {
+        $('#arraivalDate').datepicker({
+            dateFormat: 'yy/mm/dd'
+        });
+    })
+
+    Meteor.subscribe("air_lines");
+    Meteor.subscribe("flight_info");
+
     Template.flight_query.helpers({
-        air_lines: [
-            {
-                key: "1",
-                name: "羽田"
-            },
-            {
-                key: "2",
-                name: "成田"
-            },
-            {
-                key: "3",
-                name: "福岡"
-            },
-            {
-                key: "4",
-                name: "千歳"
-            },
-        ]
+        air_lines: function () {
+            return Air_lines.find();
+        }
     });
+
+    Template.flight_query.events({
+        "click #search": function (event) {
+            console.log(event.target);
+            event.preventDefault();
+            var $leavedPort = $('#leaved_port');
+            var $arrivalPort = $('#arrival_port');
+            var $leavedAt = $('#arraivalDate');
+
+            Session.set("leavedFrom", $leavedPort.val());
+            Session.set("arrivalTo", $arrivalPort.val());
+            Session.set("leavedAt", $leavedAt.val());
+//            Flight_info.find({learvedFrom:$leavedPort.val(),arrivalTo:$arrivalPort.val(),leavedAt:$leavedAt.val()});
+        }
+    });
+
 }
